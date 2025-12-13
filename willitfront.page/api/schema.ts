dac30f1583816d0099979bec6fa6d0e919acfea9
2pac <tarasyaremabcn@gmail.com>
2025-12-13 02:25:49 +0100

    test: inline constant to diagnose import issue

--- willitfront.page/api/schema.ts
+++ willitfront.page/api/schema.ts
@@ -1,5 +1,6 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
-import { HN_SQL_API } from './_lib/constants';
+
+const HN_SQL_API = process.env.HN_SQL_API_URL || 'http://localhost:3123';
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== 'GET') {
